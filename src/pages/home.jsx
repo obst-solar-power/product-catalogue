9448af294afa124730f44602b7ae7707bb8fc4bf
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2024-04-30 10:35:15 +0100

    changed select down option to street light

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -20,7 +20,7 @@ const Home = function(){
         <div className="my-5">
             <select value={type} onChange={(e)=> setType(e.target.value)} name="type" className="border border-black py-2 font-normal w-[250px] text-center rounded-md mx-auto block">
                 <option value="storage"> Storage System</option>
-                <option value="light"> Solar panels</option>
+                <option value="light"> Solar Street Lights</option>
             </select>
         </div>
             <div className="gap-y-10 px-3 grid max-sm:grid-cols-1 grid-cols-2 gap-x-10 mt-10 max-w-[1000px] mx-auto">
